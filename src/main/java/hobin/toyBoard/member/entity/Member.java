54e479d2e90125759e0592ccb75d919ee0811d7c
jumgho <103169947+jumgho@users.noreply.github.com>
2022-12-04 22:55:58 +0900

    22.12.04. Member entity 작성

--- src/main/java/hobin/toyBoard/member/entity/Member.java
+++ src/main/java/hobin/toyBoard/member/entity/Member.java
@@ -1,4 +1,54 @@
 package hobin.toyBoard.member.entity;
 
-public class Member {
+import hobin.toyBoard.audit.Auditable;
+import hobin.toyBoard.comment.entity.Comment;
+import hobin.toyBoard.like.entity.Like;
+import hobin.toyBoard.post.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+    @Column(nullable = false)
+    private String address;
+
+    @OneToMany(mappedBy = "post")
+    private List<Post> posts = new ArrayList<>();
+    @OneToMany(mappedBy = "like")
+    private List<Like> likes = new ArrayList<>();
+    @OneToMany(mappedBy = "comment")
+    private List<Comment> comments = new ArrayList<>();
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
 }
