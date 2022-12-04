0a2fec2141653ef07cba2ea3b945c5f0afd448bd
jumgho <103169947+jumgho@users.noreply.github.com>
2022-12-04 22:55:39 +0900

    22.12.04. Like entity 작성

--- src/main/java/hobin/toyBoard/like/entity/Like.java
+++ src/main/java/hobin/toyBoard/like/entity/Like.java
@@ -1,4 +1,26 @@
 package hobin.toyBoard.like.entity;
 
-public class Like {
+import hobin.toyBoard.audit.Auditable;
+import hobin.toyBoard.member.entity.Member;
+import hobin.toyBoard.post.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Like extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+    @ManyToOne
+    @JoinColumn(name = "POST_ID")
+    private Post post;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 }
